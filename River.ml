cb8b26ef124465d15b20dd1e6585578e9874d998
superdyoll <lpp1g14@soton.ac.uk>
2016-03-18 11:42:20 +0000

    It compiles

--- River.ml
+++ River.ml
@@ -4,6 +4,7 @@ exception UnboundVariableError of string;;
 exception Terminated of string;;
 exception StuckTerm of string;;
 exception NonBaseTypeResult of string;;
+exception DimensionError of string;;
 
 open Printf;;
 
@@ -508,24 +509,40 @@ let rec evalloop inStreams env e = try ( let (e',env') = (eval1M inStreams env e
 
 let evalProg inputBuffer e = evalloop (RmStream(RivStream(RivInt), (convertToStream (List.rev inputBuffer)))) (Env[]) e ;;
 
+let rec append_streams x y = match (x,y) with
+  | (Stream(a,ae), b) -> 
+           Stream(a, function() -> (append_streams (ae()) b))
+  | (StreamEnd(),b) -> b
+  | (_,StreamEnd()) -> StreamEnd()
+
 let rec count_streams streams acc = match streams with
   | Stream(n,e) -> count_streams (e()) (acc + 1)
   | StreamEnd() -> acc
 
 let rec print_streams_rec streams no_streams pos = match streams with 
-  | Stream(n,Stream(e, eR)) -> 
+  | Stream(RmStream(_,n), e) -> 
       match n with
-         | Stream(f,r) -> print_int f; if (pos mod no_streams) = 0 then print_string "\n"; print_streams_rec (Stream(e(), (function() -> r))) no_streams (pos + 1)
-         | StreamEnd() -> unit
-  | StreamEnd() -> unit
+         | Stream(RmNum(nF),nR) -> print_int nF; 
+             if (pos mod no_streams) = 0 
+                 then print_string "\n";
+             (match e() with 
+                | Stream (RmNum(eF), eR) ->
+                    print_streams_rec (Stream(RmNum(eF), (function() -> (append_streams (eR()) (Stream(RmStream(RivInt ,(nR())), function ()-> StreamEnd())))))) no_streams (pos + 1))
+         | StreamEnd() -> ()
+         | _ -> raise (DimensionError "Can only print 2D arrays")
+  | Stream(RmNum(n), e) ->
+      print_int n;
+      print_string "\n";
+      print_streams_rec (e()) no_streams pos;
+  | StreamEnd() -> ()
 
 let rec print_streams stream = 
-   (print_streams_rec streams (count_streams stream))
+   (print_streams_rec stream (count_streams stream 0) 0)
 
 and print_res res = match res with
   | RmNum (i) -> print_int i
   | RmUnit () -> print_string "() "
-  | RmStream (tT, Stream(n,e)) ->print_stream(Stream(n,e))
+  | RmStream (tT, Stream(n,e)) -> print_streams(Stream(n,e))
   | RmLbd(rT,tT,x,e) -> print_string("Function : " ^ type_to_string( typeProg (res) ))
   | _ -> raise (NonBaseTypeResult "Not able to output result as string")
 ;;
